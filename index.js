604c6fbe19069d22e943bf935519becd01e8b563
sultanabdulaev3107-ctrl <sultanabdulaev3107@gmail.com>
2026-05-22 23:05:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { Bot } from "https://deno.land/x/grammy@v1.22.4/mod.ts";
 import { webhookCallback } from "https://deno.land/x/grammy@v1.22.4/mod.ts";
 
-const bot = new Bot("ВАШ_ТОКЕН_БОТА");
+const bot = new Bot("8633981336:AAG6axtOHmnOwnVdbToFsMQqb3tzvmEqpeE");
 
 bot.command("start", (ctx) => ctx.reply("Бот успешно запущен на Cloudflare!"));
 
