f0ca874bccd8142a31ba1264d917275876bd38a0
sultanabdulaev3107-ctrl <sultanabdulaev3107@gmail.com>
2026-05-22 23:15:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import { Bot } from "https://deno.land/x/grammy@v1.22.4/mod.ts";
-import { webhookCallback } from "https://deno.land/x/grammy@v1.22.4/mod.ts";
+import { Bot, webhookCallback } from "grammy";
 
 const bot = new Bot("8633981336:AAG6axtOHmnOwnVdbToFsMQqb3tzvmEqpeE");
 
 bot.command("start", (ctx) => ctx.reply("Бот успешно запущен на Cloudflare!"));
 
-// Слушаем запросы от Telegram
 export default {
   fetch: webhookCallback(bot, "cloudflare-mod"),
 };
