4f1cf69a4e16aaca613b23b7c1278f4e7d69ff2a
sultanabdulaev3107-ctrl <sultanabdulaev3107@gmail.com>
2026-05-23 16:59:33 +0300

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -2,5 +2,5 @@
 export const ACCOUNTS = [
     {"email": "cpmcpmking1@gmail.com", "password": "666666"},
     {"email": "cpmcpmking2@gmail.com", "password": "666666"},
-    {"email": "sultanabdulaev26@gmail.com", password: "31072006" },
+    {"email": "sultanabdulaev26@gmail.com", "password": "31072006"},
 ];
