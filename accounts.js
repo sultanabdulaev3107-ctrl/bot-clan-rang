b4ed583020a5aa5c17140c3862c13602c6960976
sultanabdulaev3107-ctrl <sultanabdulaev3107@gmail.com>
2026-05-23 16:48:08 +0300

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,6 +1,4 @@
 // accounts.js
 export const ACCOUNTS = [
     { email: "sultanabdulaev26@gmail.com", password: "31072006" },
-    { email: "второй@mail.com", password: "пароль2" },
-    { email: "третий@mail.com", password: "пароль3" }
 ];
