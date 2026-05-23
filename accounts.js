c75b411050d91f118f4ceafca3d3d7d66e06d4a5
sultanabdulaev3107-ctrl <sultanabdulaev3107@gmail.com>
2026-05-23 12:04:31 +0300

    accounts.js

---
+++ accounts.js
@@ -0,0 +1,6 @@
+// accounts.js
+export const ACCOUNTS = [
+    { email: "первый@mail.com", password: "пароль1" },
+    { email: "второй@mail.com", password: "пароль2" },
+    { email: "третий@mail.com", password: "пароль3" }
+];
